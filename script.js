eeaa88d2144688fa50c7bc98d0b5003fd9fcf34e
chandraramadhan07 <rizktc@gmail.com>
2024-01-03 16:43:28 +0700

    End

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
+
+window.addEventListener("scroll", function(){   
+    var nav = document.getElementById("home-nav");
+    nav.classList.toggle("stick", window.scrollY > 830);
+})
